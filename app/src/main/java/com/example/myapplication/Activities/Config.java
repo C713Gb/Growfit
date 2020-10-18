7802633678e6c0347af2854b2492a7106a1448c5
Raorakshith <raorakshith990@gmail.com>
2020-10-18 21:11:37 +0530

    small changes

--- app/src/main/java/com/example/myapplication/Activities/Config.java
+++ app/src/main/java/com/example/myapplication/Activities/Config.java
@@ -1,6 +1,6 @@
 package com.example.myapplication.Activities;
 
 public class Config {
-    public static final String DEVELOPER_KEY="AlzaSyBh9rJznRuRmcKl8Xfxb8reBcrbLZZjM-E";
+    public static final String DEVELOPER_KEY="AIzaSyCKspgKrQzeGNdFx17xbNKt8oZqz_sUoE8";
     public static final String YOUTUBE_VIDEO_CODE="_oEA18Y8gM0";
 }

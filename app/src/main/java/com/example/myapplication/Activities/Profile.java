45f34469b6299314eb5fbe9394e41cb2e5bc99b5
Gourav Banerjee <39580718+C713Gb@users.noreply.github.com>
2020-10-18 19:31:06 +0530

    Profile Updated

---
+++ app/src/main/java/com/example/myapplication/Activities/Profile.java
@@ -0,0 +1,77 @@
+package com.example.myapplication.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.myapplication.Models.User;
+import com.example.myapplication.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Profile extends AppCompatActivity {
+
+    TextView name, email;
+    FirebaseAuth auth;
+    DatabaseReference dref;
+    Button edit;
+    ImageButton back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        name = findViewById(R.id.username_txt);
+        email = findViewById(R.id.email_txt_3);
+        edit = findViewById(R.id.complete_profile_btn);
+        back = findViewById(R.id.back_2);
+
+        auth = FirebaseAuth.getInstance();
+
+        dref = FirebaseDatabase.getInstance().getReference("Users");
+        dref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                    User user = snapshot1.getValue(User.class);
+                    String s_name = user.getUsername();
+                    String s_email = user.getEmail();
+                    name.setText(s_name);
+                    email.setText(s_email);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Profile.this, EditProfile.class);
+                startActivity(intent);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
